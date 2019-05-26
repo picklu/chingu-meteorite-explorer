b14746d04fa88c0ffcf1aa724dd8b426ed460daa
Subrata Sarker <subrata_sarker@yahoo.com>
2019-05-26 12:44:13 +0900

    Update webapck.config.js to minify html in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, './public/index.html'),
-      favicon: path.resolve(__dirname, './public/media/favicon.ico')
+      favicon: path.resolve(__dirname, './public/media/favicon.ico'),
+      minify: {
+        collapseWhitespace: true
+      }
     })
   ],
   module: {
