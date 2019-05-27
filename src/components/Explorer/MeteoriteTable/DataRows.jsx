bc737f4679702aa7f7e85200df3386e2a58203e5
Subrata Sarker <subrata_sarker@yahoo.com>
2019-05-27 12:44:47 +0900

    Directly use props.filterText to create regx

--- src/components/Explorer/MeteoriteTable/DataRows.jsx
+++ src/components/Explorer/MeteoriteTable/DataRows.jsx
@@ -3,9 +3,8 @@ import DataRow from './DataRow';
 
 export default class DataRows extends Component {
   render() {
-    const filterText = this.props.filterText;
+    const regx = new RegExp(this.props.filterText, 'gi');
     const category = this.props.category;
-    const regx = new RegExp(filterText, 'gi');
     const rows = [];
     let rowNum = 0;
     this.props.meteorites.forEach(meteorite => {
