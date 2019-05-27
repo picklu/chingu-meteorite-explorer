99e77babe6f3c4013871ef4484dcf2657e816c4e
Subrata Sarker <subrata_sarker@yahoo.com>
2019-05-28 00:21:49 +0900

    Style inputbox

--- src/components/Explorer/MeteoriteSearch/index.jsx
+++ src/components/Explorer/MeteoriteSearch/index.jsx
@@ -13,13 +13,19 @@ export default class MeteoriteSearch extends Component {
 
   render() {
     return (
-      <form>
-        <input
-          type="text"
-          placeholder="Search a meteorite ..."
-          value={this.props.filterText}
-          onChange={this.handleFilterTextChange}
-        />
+      <form className="form">
+        <div class="row form__row">
+          <div class="form__group form__group--input">
+            <input
+              id="query"
+              type="text"
+              class="form__control--input"
+              placeholder="Search a meteorite ..."
+              value={this.props.filterText}
+              onChange={this.handleFilterTextChange}
+            />
+          </div>
+        </div>
       </form>
     );
   }
