5846cd9e8651b81aa5e058b065dd8ec06115da9c
Subrata Sarker <subrata_sarker@yahoo.com>
2019-06-09 13:21:50 +0900

    Use enzyme-to-json serializer

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, { shallow } from 'enzyme';
 import App from '../App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test('App snapshot test', () => {
-  const component = renderer.create(<App />);
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Test App', () => {
+  const component = shallow(<App />);
+  it('App snapshot test', async done => {
+    expect(component).toMatchSnapshot();
+    done();
+  });
 });
